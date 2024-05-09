51fe4c73b312ace06d7d6d4acb84054d789bf909
Ivirson Daltro <ivi.daltro@gmail.com>
2024-05-09 11:23:48 -0300

    Api para gerenciamento de consultas

---
+++ modules/appointments/routes/appointments.routes.js
@@ -0,0 +1,13 @@
+const { Router } = require("express");
+const appointmentsController = require("../controllers/appointments.controller");
+const verifyToken = require("../../../middlewares/verify-token");
+
+const appointmentsRouter = Router();
+
+appointmentsRouter.get("/", verifyToken, appointmentsController.findAll);
+appointmentsRouter.get("/:id", verifyToken, appointmentsController.findById);
+appointmentsRouter.post("/", verifyToken, appointmentsController.create);
+appointmentsRouter.put("/:id", verifyToken, appointmentsController.update);
+appointmentsRouter.delete("/:id", verifyToken, appointmentsController.delete);
+
+module.exports = appointmentsRouter;
