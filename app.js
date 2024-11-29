01d32c55e10e7880cfbf1482d9c1ccae016e470b
Vitor Diorio <vitor.diorio@yahoo.com.br>
2024-11-28 21:35:03 -0300

    Add PORT env

--- app.js
+++ app.js
@@ -14,6 +14,8 @@ app.use(express.json());
 app.use(router);
 app.use("/doc", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
-app.listen(3000, () => console.log("Server running on port 3000"));
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 module.exports = app;
